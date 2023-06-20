09bbaa92366480633d46288ad9ff5b3a5eb5bddf
kdunorat <kdu.norat@gmail.com>
2023-06-20 19:44:30 -0300

    Implementing CLI

--- cath_cli.py
+++ cath_cli.py
@@ -1,5 +1,30 @@
-from sys import argv
-from sys import exit
-from sys import stdin
-from sys import stderr
-from collections import deque
+import sys
+from typing import List
+from cath_request import CathRequest
+
+
+class CathCli:
+    def __init__(self):
+        self.args: List[str] = sys.argv[1:]
+        self.usage = ''
+
+    def cli(self):
+        #funfam = self.args[1]
+
+        if len(self.args) > 2:
+            print(f"The maximum number of arguments is 2, you inputed {len(self.args)} arguments")
+            print(self.usage)
+            sys.exit()
+        if len(self.args) < 1:
+            print("You must put at least 1 argument")
+            print(self.usage)
+            sys.exit()
+        elif len(self.args) == 1:
+            superfamily_id = self.args[0]
+            cath = CathRequest(superfamily_id)
+            cath.get_info()
+
+
+if __name__ == '__main__':
+    cli_tool = CathCli()
+    cli_tool.cli()
--- cath_request.py
+++ cath_request.py
@@ -14,10 +14,16 @@ class CathRequest:
         
         # BS4
         soup = BeautifulSoup(r1.content, "html.parser")
-        with open("arquivo.html", "w", encoding="utf-8") as file:
-            file.write(str(soup))
+        if 'CATH: Internal Error' in soup.text:
+            raise CathInternalError(f"An error has ocurred when retrieving the superfamily - {self.superfamily_id}"
+                                    f" information from CATH database, verify if"
+                                    f" you are putting the correct superfamily id")
+
+        with open("arquivo.html", "w", encoding="utf-8") as f:
+            f.write(str(soup))
         all_a = soup.find_all('a')
-        
+        funfam_menu = f"FUNCTIONAL FAMILIES OF SUPERFAMILY: {self.superfamily_id}\n"
+        print(soup.text)
         for a in all_a[17:]:
             if 'CATH News' in a.text:
                 break
@@ -30,10 +36,23 @@ class CathRequest:
             elif fam_name == '':
                 fam_name = 'N/A'
             if file_name == 'funfam' and fam_id not in fam_set:
-                print(f"{fam_id} ---- {fam_name}")
+                funfam_menu += f"{fam_id} ---- {fam_name}\n"
                 fam_set.add(fam_id)
-                
-    def get_fam(self, funfam_id):
+        print(funfam_menu)
+        self.select_family(fam_set)
+
+    def select_family(self, fam_set: set):
+        input_number = input("Select funfam number:")
+        if not input_number.isnumeric() or input_number.isdecimal():
+            print("You must put a natural number representative of a functional family")
+        elif input_number not in fam_set:
+            print(f'The input: {input_number} is not a functional family in the superfamily: {self.superfamily_id}')
+
+        else:
+            print("Certo")
+
+
+    def get_fam(self, funfam_id: str | int):
         url_2 = f"http://www.cathdb.info/version/latest/superfamily/{self.superfamily_id}/funfam/{funfam_id}/alignment"
         r2 = self.client.get(url=url_2, timeout=50)
         soup2 = BeautifulSoup(r2.content, "html.parser")
@@ -42,7 +61,7 @@ class CathRequest:
         
         return stock_file, funfam_id
     
-    def get_species_file(self, stock_file, funfam_id):
+    def get_species_file(self, stock_file, funfam_id: str):
         r3 = self.client.get(url=stock_file, timeout=50)
         soup3 = BeautifulSoup(r3.content, "html.parser")
         new_file_name = f"{self.superfamily_id}:{funfam_id}-Species.txt"
@@ -51,8 +70,9 @@ class CathRequest:
         self.species_names(new_file_name)
     
     @staticmethod
-    def species_names(path):
+    def species_names(path: str):
         specs = []
+        sps_verified = set()
         with open(path, 'r') as f:
             content = f.readlines()
             for line in content:
@@ -61,8 +81,10 @@ class CathRequest:
                     spec = spec[-1]
                     specs.append(spec)
         for sp in specs:
-            print(sp)
-        
+            if sp not in sps_verified:
+                print(sp)
+                sps_verified.add(sp)
+
     @staticmethod
     def get_file_name(href: str, house: int):
         file_name = href.split("/")
@@ -73,11 +95,16 @@ class CathRequest:
             return False
     
 
+class CathInternalError(Exception):
+    pass
+
+
 if __name__ == '__main__':
     id1 = '1.10.1780.10'
     id2 = '1.10.510.10'
-    inst = CathRequest(id1)
-    file, id_fam = inst.get_fam(16)
-    inst.get_species_file(file, id_fam)
+    inst = CathRequest('mablibsoibnsoi')
+    inst.get_info()
+    #file, id_fam = inst.get_fam(6)
+    #inst.get_species_file(file, id_fam)
     
     
