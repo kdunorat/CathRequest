270ba04603d35d8d02827a3bd7b6c52980daeee4
kdunorat <kdu.norat@gmail.com>
2023-06-20 21:53:58 -0300

    Cli implemented

--- cath_cli.py
+++ cath_cli.py
@@ -1,30 +1,52 @@
 import sys
+import re
 from typing import List
 from cath_request import CathRequest
 
 
 class CathCli:
     def __init__(self):
-        self.args: List[str] = sys.argv[1:]
+        if len(sys.argv) < 2:
+            print("Invalid Input")
+            sys.exit()
+        else:
+            self.args: List[str] = sys.argv[1:]
         self.usage = ''
 
     def cli(self):
-        #funfam = self.args[1]
-
+        superfamily_id = self.args[0]
+        cath = CathRequest(superfamily_id)
+        if not _input_pattern(superfamily_id):
+            print("Invalid Superfamily ID")
+            sys.exit()
         if len(self.args) > 2:
             print(f"The maximum number of arguments is 2, you inputed {len(self.args)} arguments")
             print(self.usage)
             sys.exit()
-        if len(self.args) < 1:
+        elif len(self.args) < 1:
             print("You must put at least 1 argument")
             print(self.usage)
             sys.exit()
         elif len(self.args) == 1:
-            superfamily_id = self.args[0]
-            cath = CathRequest(superfamily_id)
-            cath.get_info()
+            cath.run(opt=1, funfam_id=0)
+        else:
+            try:
+                funfam = int(self.args[1])
+                cath.run(opt=2, funfam_id=funfam)
+            except ValueError:
+                print("You must put a number representative of a functional family for this superfamily")
+
+
+def _input_pattern(string):
+    pattern = r'^[0-9.]+$'
+    match = re.match(pattern, string)
+    if match:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
     cli_tool = CathCli()
     cli_tool.cli()
+    
--- cath_request.py
+++ cath_request.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 class CathRequest:
@@ -7,7 +8,7 @@ class CathRequest:
         self.superfamily_id = superfamily_id
         self.client = requests.session()
         
-    def get_info(self):
+    def _get_info(self):
         fam_set = set()
         url_1 = f"http://www.cathdb.info/version/latest/superfamily/{self.superfamily_id}/alignments" 
         r1 = self.client.get(url=url_1, timeout=50)
@@ -23,70 +24,91 @@ class CathRequest:
             f.write(str(soup))
         all_a = soup.find_all('a')
         funfam_menu = f"FUNCTIONAL FAMILIES OF SUPERFAMILY: {self.superfamily_id}\n"
-        print(soup.text)
         for a in all_a[17:]:
             if 'CATH News' in a.text:
                 break
             path = a["href"]
-            file_name = self.get_file_name(path, 2)
-            fam_id = self.get_file_name(path, 1)
+            file_name = self._get_file_name(path, 2)
+            fam_id = self._get_file_name(path, 1)
             fam_name = a.text
             if fam_name.isnumeric():
                 continue
             elif fam_name == '':
                 fam_name = 'N/A'
             if file_name == 'funfam' and fam_id not in fam_set:
-                funfam_menu += f"{fam_id} ---- {fam_name}\n"
-                fam_set.add(fam_id)
+                funfam_menu += f"  {fam_id} ----- {fam_name}\n"
+                fam_set.add(int(fam_id))
         print(funfam_menu)
-        self.select_family(fam_set)
+        self._select_family(fam_set)
 
-    def select_family(self, fam_set: set):
-        input_number = input("Select funfam number:")
-        if not input_number.isnumeric() or input_number.isdecimal():
-            print("You must put a natural number representative of a functional family")
-        elif input_number not in fam_set:
-            print(f'The input: {input_number} is not a functional family in the superfamily: {self.superfamily_id}')
+    def _select_family(self, fam_set: set):
+        while True:
+            try:
+                input_number = int(input("\n\nSelect funfam number:"))
+                if input_number in fam_set:
+                    break
+                else:
+                    print(f'The input: {input_number} is not a functional'
+                          f'family in the superfamily: {self.superfamily_id}')
+                    time.sleep(1.5)
 
-        else:
-            print("Certo")
+            except ValueError:
+                print("You must put a number representative of a functional family for this superfamily")
+        stock, funfam_name = self._get_fam(input_number)
+        self._get_species_file(stock, input_number, funfam_name)
 
-
-    def get_fam(self, funfam_id: str | int):
+    def _get_fam(self, funfam_id: int):
         url_2 = f"http://www.cathdb.info/version/latest/superfamily/{self.superfamily_id}/funfam/{funfam_id}/alignment"
         r2 = self.client.get(url=url_2, timeout=50)
         soup2 = BeautifulSoup(r2.content, "html.parser")
+        funfam_name = ''
+        has_name = False
+        for name in soup2.find_all('h2'):
+            if 'FunFam' in name.text:
+                funfam_name = name.text.split(':')
+                funfam_name = funfam_name[-1]
+                if funfam_name == '':
+                    has_name = True
+        if not has_name:
+            funfam_name = funfam_id
+
+        if '(en) Please come back later' in soup2.text:
+            raise CathInternalError(f"An error has ocurred when retrieving the functional family"
+                                    f" information from CATH database, verify if"
+                                    f" you are putting the correct funfam id")
         classe = soup2.find('a', class_="btn btn-small btn-info")
         stock_file = classe['href']
         
-        return stock_file, funfam_id
+        return stock_file, funfam_name
     
-    def get_species_file(self, stock_file, funfam_id: str):
+    def _get_species_file(self, stock_file, funfam_id: int, funfam_name: str):
         r3 = self.client.get(url=stock_file, timeout=50)
         soup3 = BeautifulSoup(r3.content, "html.parser")
         new_file_name = f"{self.superfamily_id}:{funfam_id}-Species.txt"
         with open(new_file_name, "w", encoding="utf-8") as f:
             f.write(str(soup3))
-        self.species_names(new_file_name)
-    
-    @staticmethod
-    def species_names(path: str):
-        specs = []
-        sps_verified = set()
-        with open(path, 'r') as f:
-            content = f.readlines()
-            for line in content:
-                if 'OS' in line:
-                    spec = line.rsplit('OS')
-                    spec = spec[-1]
-                    specs.append(spec)
-        for sp in specs:
-            if sp not in sps_verified:
-                print(sp)
-                sps_verified.add(sp)
+        self._species_names(new_file_name, funfam_name)
 
     @staticmethod
-    def get_file_name(href: str, house: int):
+    def _species_names(path: str, funfam_name: str):
+        specs = []
+        sps_verified = set()
+        sp_msg = f'\n------- UNIQUE SPECIES FROM: {funfam_name} -------\n'
+        with open(path, 'r') as f:
+            content = f.readlines()
+        for line in content:
+            if 'OS' in line:
+                spec = line.rsplit('OS')
+                spec = spec[-1]
+                specs.append(spec)
+        for sp in specs:
+            if sp not in sps_verified:
+                sp_msg += f"{sp}\n"
+                sps_verified.add(sp)
+        print(sp_msg)
+
+    @staticmethod
+    def _get_file_name(href: str, house: int):
         file_name = href.split("/")
         try:
             file_name = file_name[-house]
@@ -94,6 +116,13 @@ class CathRequest:
         except IndexError:
             return False
     
+    def run(self, opt: int, funfam_id: int):
+        if opt == 1:
+            self._get_info()
+        elif opt == 2:
+            stock, funfam_name = self._get_fam(funfam_id)
+            self._get_species_file(stock, funfam_id, funfam_name)
+
 
 class CathInternalError(Exception):
     pass
@@ -102,9 +131,6 @@ class CathInternalError(Exception):
 if __name__ == '__main__':
     id1 = '1.10.1780.10'
     id2 = '1.10.510.10'
-    inst = CathRequest('mablibsoibnsoi')
-    inst.get_info()
-    #file, id_fam = inst.get_fam(6)
-    #inst.get_species_file(file, id_fam)
-    
+    inst = CathRequest(id2)
+    inst.run(1, 16)
     
